defef4b83be133e619654097d37c47d4d37d9c65
Hafiz Kurniawan <99454377+hafizen@users.noreply.github.com>
2023-09-03 23:43:56 +0700

    Update A01-basic-filtering-with-sakila.sql

--- A01-basic-filtering-with-sakila.sql
+++ A01-basic-filtering-with-sakila.sql
@@ -31,36 +31,41 @@ select film_id, title, rental_rate, release_year
 from film
 where release_year < 2006
 
+-- 7
 select *
 from rental
 where rental_date like '2006%'
 order by rental_date
 
-
+-- 8
 select first_name, last_name
 from sakila.actor
 where last_name = 'LOLLOBRIGIDA'
 
+ -- 9
 select title
 from sakila.film
 where replacement_cost >= 20
 order by title
 
+-- 10
 SELECT *
 from sakila.film
 where rental_duration <= 4
 order by film_id
 
+-- 11
 SELECT title, rental_rate, length
 from sakila.film
 where length > 120
 order by length
 
+-- 12
 SELECT title, rental_rate
 from sakila.film
 where rental_rate <= 1.5
 
+-- 13
 select title, length
 from sakila.film
 where length > 120
-
